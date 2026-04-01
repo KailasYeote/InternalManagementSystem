a2ac4f0df6665a062f30ec27563fc389fa90aa72
KailasYeote <yeotekailas65@gmail.com>
2026-04-02 01:44:05 +0530

    fix: set base path to '/' and clean up vite config and package.json

--- internalmanagementsystem/ims-ims/vite.config.js
+++ internalmanagementsystem/ims-ims/vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
-  base: '/InternalManagementSystem/', //
+  base: '/',
   plugins: [react()],
-  resolve: {
-    dedupe: ['react', 'react-dom', 'react-router-dom'], // 👈 add this
-  },
   server: {
     port: 3000
   },
